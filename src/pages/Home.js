37dc7e05efc0aacf24043b30edf05f940080e6d1
Ben Tucker <bwtucker4@gmail.com>
2022-08-31 04:52:03 -0700

    Continue bulding layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,8 +35,10 @@ const Home = () => {
                 <div className="block"></div>
                 <h4>WHO WE ARE</h4>
                 <br></br>
-                <div>Innovation through integration</div>
-                <p>
+                <div className="about-us-header">
+                    Innovation through integration
+                </div>
+                <p className="company-bio">
                     Omicron offers full development, design and construction
                     services to maximize value for our clients. With all
                     professionals under one roof, we compete more effectively in
@@ -44,7 +46,43 @@ const Home = () => {
                     efficiency. The benefits are enhanced by how we leverage a
                     host of technologies in all our projects.
                 </p>
+                <aside className="approaches">
+                    <ul>
+                        <li>
+                            <div className="approach-number">01.</div>
+                            <p className="approach-description">Development</p>
+                        </li>
+                        <li>
+                            <div className="approach-number">02.</div>
+                            <p className="approach-description">Architecture</p>
+                        </li>
+                        <li>
+                            <div className="approach-number">03.</div>
+                            <p className="approach-description">
+                                Interior Design
+                            </p>
+                        </li>
+                        <li>
+                            <div className="approach-number">04.</div>
+                            <p className="approach-description">Engineering</p>
+                        </li>
+                        <li>
+                            <div className="approach-number">05.</div>
+                            <p className="approach-description">Maintenance</p>
+                        </li>
+                        <li>
+                            <div className="approach-number">06.</div>
+                            <p className="approach-description">Construction</p>
+                        </li>
+                    </ul>
+                </aside>
             </section>
+            <div className="featured-project">
+                <img
+                    className="project-image"
+                    src={require("../images/modern-office.jpeg")}
+                ></img>
+            </div>
         </>
     );
 };
